82adda01c47700cd97084f7a064c26397b3aec03
Evgeniy Loznevoy <evgeniy.loznevoy@yandex.ru>
2022-10-04 13:53:33 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/jmvgk8d366l7161s?svg=true)](https://ci.appveyor.com/project/evgeniyloznevoy/ahj-homeworks-events)
+
+https://evgeniyloznevoy.github.io/ahj-homeworks-events
---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+// TODO: write code here
+
+import Board from './Board';
+import Goblin from './Goblin';
+
+const cells = document.getElementsByClassName('cell');
+const eScores = document.getElementById('scores');
+const eMisses = document.getElementById('misses');
+const pop = document.getElementsByClassName('game_end')[0];
+const button = document.getElementsByClassName('button')[0];
+
+const goblin = new Goblin();
+const board = new Board(goblin, eScores, eMisses, pop);
+board.init(cells, button);
